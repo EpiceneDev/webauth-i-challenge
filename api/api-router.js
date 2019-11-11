ef726c07f98d4d9f14b9eb6b38dfcead255ea947
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-11-11 14:54:29 -0800

    password successfully hashed

--- api/api-router.js
+++ api/api-router.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 
 const authRouter = require('../auth/auth-router.js');
@@ -9,5 +10,17 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
     res.json({ api: "It's alive" });
 });
+
+router.post('/hash', (req, res) => {
+    // read a password from the body
+    const password = req.body.password;
+
+    // hash the password using bcryptjs 8 - 
+    const hash = bcrypt.hashSync(password, 8);
+
+    // return it to the user in an object that looks like
+    // { password: 'original passsword', hash: 'hashed password' }
+    res.status(200).json({ password, hash });
+})
 
 module.exports = router;
