50d08d91bf20b5f1238805d3c3d6be8d61751bb4
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-11-12 14:11:55 -0800

    mvp met

--- auth/requires-auth-middleware.js
+++ auth/requires-auth-middleware.js
@@ -1,34 +1,7 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
-
 module.exports = (req, res, next) => {
   if (req.session && req.session.username) {
     next();
   } else {
     res.status(401).json({ you: 'can not enter' });
   };
-  //BELOW IS FOR AUTH, ABOVE IS FOR COOKIES/SESSIONS
-  //AND YOU SHOULD CHNGE NAME OF FILE TO restricted-middleware
-  // const { username, password } = req.headers; // not from body so it works on GET
-
-  // if (username && password) {
-  //   Users.findBy({ username })
-  //     .first()
-  //     .then(user => {
-  //       if (user && bcrypt.compareSync(password, user.password)) {
-  //         next(); // this is different from the login code
-  //       } else {
-  //         res.status(401).json({ message: 'Invalid Credentials' });
-  //       }
-  //     })
-  //     .catch(error => {
-  //       console.log('login error', error);
-  //       res
-  //         .status(500)
-  //         .json({ message: 'ran into an error, please try later' });
-  //     });
-  // } else {
-  //   res.status(400).json({ message: 'please provide credentials' });
-  // }
 };
